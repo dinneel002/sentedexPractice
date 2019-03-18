1629968ddcaa0573b8c7fef53b6c15d521c6d4cc
dinneel002 <dneel002@gmail.com>
2019-03-19 00:20:17 +0530

    re_

--- mySite/mySite/urls.py
+++ mySite/mySite/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path('Webapp/', include('Webapp.urls'))
+    path('Webapp/', include('Webapp.urls'))
 ]
